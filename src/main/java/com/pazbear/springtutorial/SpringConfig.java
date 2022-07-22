3486b3763228e44a4bc6ad9adfd50d8570b6c8dd
mkcho1997 <mkcho1997@mkcho1997ui-MacBookAir.local>
2022-07-22 20:33:30 +0900

    25 스프링 데이터 JPA

--- src/main/java/com/pazbear/springtutorial/SpringConfig.java
+++ src/main/java/com/pazbear/springtutorial/SpringConfig.java
@@ -16,6 +16,14 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
     @PersistenceContext
     private EntityManager em;
 
@@ -23,6 +31,7 @@ public class SpringConfig {
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+    */
 
     /*
     private DataSource dataSource;
@@ -37,14 +46,16 @@ public class SpringConfig {
      */
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
+    /*
     @Bean
     public MemberRepository memberRepository(){
 
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        //return new JpaMemberRepository(em);
     }
+    */
 }
