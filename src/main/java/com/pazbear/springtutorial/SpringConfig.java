d7bd98a440724fb4ba724e72c66ebb9e91e812e6
mkcho1997 <mkcho1997@mkcho1997ui-MacBookAir.local>
2022-07-22 18:25:57 +0900

    21 순수 JDBC

--- src/main/java/com/pazbear/springtutorial/SpringConfig.java
+++ src/main/java/com/pazbear/springtutorial/SpringConfig.java
@@ -1,13 +1,25 @@
 package com.pazbear.springtutorial;
 
+import com.pazbear.springtutorial.repository.JdbcMemberRepository;
 import com.pazbear.springtutorial.repository.MemberRepository;
 import com.pazbear.springtutorial.repository.MemoryMemberRepository;
 import com.pazbear.springtutorial.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     /**
      * 자바 코드로 직접 스프링 빈 등록
      */
@@ -18,6 +30,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
